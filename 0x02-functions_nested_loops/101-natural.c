f8001716873a9abc4e3db7a2adfb248dae8f1624
JCZams1 <jessezamani456@gmail.com>
2023-07-18 21:08:59 -0700

    13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		sum = sum + i;
+	}
+	printf("%d\n", sum);
+}
