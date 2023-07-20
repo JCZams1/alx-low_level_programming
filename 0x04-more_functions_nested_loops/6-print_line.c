c1d9809fed444ec6dbf578588f37431d1014da7d
JCZams1 <jessezamani456@gmail.com>
2023-07-20 16:50:15 -0700

    line draw

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line
+ * @n: number of times
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
