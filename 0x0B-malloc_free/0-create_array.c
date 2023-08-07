0213a692198ce79235d4327bfa0d37505d435f74
JCZams1 <jessezamani456@gmail.com>
2023-08-07 01:29:42 -0700

    print program name

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,10 +11,12 @@ char *create_array(unsigned int size, char c)
 	char *ptrArray;
 	unsigned int count;
 
+	if (size == 0)
+		return (NULL);
 	ptrArray = malloc(size * sizeof(char));
+	if (ptrArray == 0)
+		return (NULL);
 	for (count = 0; count < size; count++)
-	{
 		ptrArray[count] = c;
-	}
 	return (ptrArray);
 }
