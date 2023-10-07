11f57a9396831df7ef0dc2668d8353b173270c1e
JCZams1 <jessezamani456@gmail.com>
2023-10-07 10:12:14 -0700

    More Malloc and Free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,53 +1,47 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of the string
- * @s: input string to count
- * Description: returns the length of a given string
- * Return: length of string as int
- **/
-int _strlen(char *s)
-{
-	unsigned int i;
+ * string_nconcat - concatenates two strings
+ * @s1: 1st string
+ * @s2: 2nd string
+ * @n: number of bytes
+ * Return: new string
+ */
 
-	if (s == NULL)
-		return (1);
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (i + 1);
-}
-/**
- * string_nconcat - concatenate n chars of second string to first
- * @s1: first string
- * @s2: second string
- * @n: number of chars to concatenate
- * Description: concatenate two strings, return NULL if fails
- * Return: pointer to string, NULL if fails
- **/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *dest;
-	unsigned int size1, size2, i, j;
+	unsigned int i;
+	unsigned int lens1 = 0;
+	unsigned int lens2 = 0;
+	char *output;
 
-	size1 = _strlen(s1);
-	size2 = _strlen(s2);
-	if (n > size2 - 1)
-		n = size2 - 1;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+		lens1++;
+	for (i = 0; s2[i] != '\0'; i++)
+		lens2++;
 
-	dest = (char *)malloc((size1 + n) * sizeof(char));
-	if (dest == NULL)
+	output = malloc(sizeof(char) * (lens1 + n) + 1);
+	if (output == NULL)
 		return (NULL);
-	if (size1 == 1 && size2 == 1)
-		return (NULL);
-
-	for (i = 0; i < size1 - 1; i++)
-		dest[i] = s1[i];
-
-	for (j = 0; j <= n; j++)
+	if (n >= lens2)
 	{
-		dest[i + j] = s2[j];
+		for (i = 0; s1[i] != '\0'; i++)
+			output[i] = s1[i];
+		for (i = 0; s2[i] != '\0'; i++)
+			output[lens1 + i] = s2[i];
+		output[lens1 + i] = '\0';
 	}
-	dest[i + j] = '\0';
-
-	return (dest);
+	else
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			output[i] = s1[i];
+		for (i = 0; i < n; i++)
+			output[lens1 + i] = s2[i];
+		output[lens1 + i] = '\0';
+	}
+	return (output);
 }
