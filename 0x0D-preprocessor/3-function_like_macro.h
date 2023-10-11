de9989b538d262902de704c051497f61d3954059
JCZams1 <jessezamani456@gmail.com>
2023-10-10 17:25:31 -0700

    Preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-
-#define ABS(x) ((x) > (-1) ? (x) : (x * -1))
-#endif /*FUNCTION_LIKE_MACRO_H*/
+#ifndef _HEADER_
+#define _HEADER_
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
+#endif
